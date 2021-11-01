d916565ff93114f5c2ca6d94b47a21632533cca7
memoer <hanjn2842@naver.com>
2021-11-02 02:11:55 +0900

    README.md 수정

--- src/baekjoon/recursive/I_P1074.java
+++ src/baekjoon/recursive/I_P1074.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 // 재귀 풀 때, 모든 경우의 수를 재귀로 돌리려 하지 말아라! 이것이 함정이다.
 // 최대한 수를 줄일 것!!!
-public class P1074 {
+public class I_P1074 {
   public static int answer = -1;
 
   public static int getAnswer(int N, int r, int c) {
