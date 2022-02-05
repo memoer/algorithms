ea2f33583c21aedd54f7158a30ff240020d6188e
memoer <hanjn2842@naver.com>
2022-02-05 12:15:14 +0900

    [프로그래머스] 그래프-가장 먼 노드(49189) 복기
    - 기존의 풀었던 코드와 다르게 풀었음

--- src/programmers/graph/G49189.java
+++ src/programmers/graph/G49189.java
@@ -1,68 +1,76 @@
 package programmers.graph;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
 public class G49189 {
-  static class Solution {
-    private Map<Integer, List<Integer>> graph = new HashMap<>();
-    private Map<Integer, Integer> path = new HashMap<>();
-    private final int START_NODE = 1;
-    private int max = 0;
-
-    private void putListToGraph(int a, int b) {
-      if (!graph.containsKey(a)) {
-        List<Integer> list = new ArrayList<>();
-        list.add(b);
-        graph.put(a, list);
-      } else {
-        graph.get(a).add(b);
-      }
-    }
-
-    private void initGraph(int[][] edge) {
-      for (int[] v : edge) {
-        putListToGraph(v[0], v[1]);
-        putListToGraph(v[1], v[0]);
-      }
-    }
-
-    private void initPath() {
-      for (int node : graph.keySet()) {
-        path.put(node, node == START_NODE ? 0 : Integer.MAX_VALUE);
-      }
-    }
-
-    public int solution(int n, int[][] edge) {
-      Queue<Integer> q = new LinkedList<>();
-      initGraph(edge);
-      initPath();
-      q.offer(START_NODE);
-      while (!q.isEmpty()) {
-        int curNode = q.poll();
-        int curPath = path.get(curNode);
-        int sumPath = curPath + 1;
-        for (int adjNode : graph.get(curNode)) {
-          if (sumPath < path.get(adjNode)) {
-            path.put(adjNode, sumPath);
-            q.offer(adjNode);
-            if (sumPath > max) {
-              max = sumPath;
-            }
-          }
-        }
-      }
-      return (int) path.values().stream().filter(x -> x == max).count();
-    }
-  }
-
   public static void main(String[] args) {
     int n = 6;
     int[][] edge = { { 3, 6 }, { 4, 3 }, { 3, 2 }, { 1, 3 }, { 1, 2 }, { 2, 4 }, { 5, 2 } };
     System.out.println(new Solution().solution(n, edge));
+  }
+
+  static class Solution {
+    public final int START_NODE = 1;
+    private Queue<Integer> q;
+    private Map<Integer, Queue<Integer>> graph;
+    private int[] dist;
+    private int max;
+
+    private void initialize(int n, int[][] vertex) {
+      this.q = new LinkedList<>();
+      q.add(this.START_NODE);
+
+      this.graph = new HashMap<>();
+      for (int[] edge : vertex) {
+        int nodeA = edge[0];
+        int nodeB = edge[1];
+        if (!graph.containsKey(nodeA)) {
+          graph.put(nodeA, new LinkedList<>());
+        }
+        if (!graph.containsKey(nodeB)) {
+          graph.put(nodeB, new LinkedList<>());
+        }
+        graph.get(nodeA).offer(nodeB);
+        graph.get(nodeB).offer(nodeA);
+      }
+
+      final int START_NODE_DISTANCE = 1;
+      this.dist = new int[n + 1];
+      this.dist[this.START_NODE] = START_NODE_DISTANCE;
+      this.max = START_NODE_DISTANCE;
+    }
+
+    private void bfs() {
+      while (!this.q.isEmpty()) {
+        int curNode = q.poll();
+        while (!this.graph.get(curNode).isEmpty()) {
+          int adjNode = this.graph.get(curNode).poll();
+          if (this.dist[adjNode] == 0) {
+            this.dist[adjNode] += this.dist[curNode] + 1;
+            this.q.offer(adjNode);
+          }
+        }
+        this.max = this.dist[curNode];
+      }
+    }
+
+    private int getResult(int n) {
+      int result = 0;
+      for (int num : this.dist) {
+        if (num == this.max) {
+          result++;
+        }
+      }
+      return result;
+    }
+
+    public int solution(int n, int[][] vertex) {
+      initialize(n, vertex);
+      bfs();
+      return getResult(n);
+    }
   }
 }
