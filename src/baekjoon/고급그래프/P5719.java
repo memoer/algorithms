82871be98883b66b186a01374d4b1b6193943e0e
memoer <hanjn2842@naver.com>
2022-02-25 11:16:32 +0900

    [백준] 고급그래프>거의 최단 경로
    - BFS말고 백트래킹으로 그래프 역추적해야 메모리초과 안뜸

--- src/baekjoon/고급그래프/P5719.java
+++ src/baekjoon/고급그래프/P5719.java
@@ -6,37 +6,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class P5719 {
   private static int[] dist;
   private static List<Node>[] graph;
   private static boolean[][] dropped;
-  private static List<Node>[] reverseGraph;
+  private static List<Integer>[] targetGraph;
 
-  private static void dijkstra(final int N, final int START) {
+  private static void dijkstra(final int N, final int start) {
     for (int i = 0; i < N; i++) {
       dist[i] = Integer.MAX_VALUE;
     }
-    dist[START] = 0;
+    dist[start] = 0;
 
     PriorityQueue<Node> pq = new PriorityQueue<>();
-    pq.offer(new Node(START, 0));
+    pq.offer(new Node(start, 0));
 
     while (!pq.isEmpty()) {
       Node cur = pq.poll();
-      if (graph[cur.idx] == null) {
+      if (dist[cur.idx] < cur.weight || graph[cur.idx] == null) {
         continue;
       }
       for (Node adj : graph[cur.idx]) {
         if (dropped[cur.idx][adj.idx]) {
           continue;
         }
-        int sum = cur.path + adj.path;
-        if (dist[adj.idx] > sum) {
+        int sum = cur.weight + adj.weight;
+        if (sum == dist[adj.idx]) {
+          targetGraph[adj.idx].add(cur.idx);
+        } else if (sum < dist[adj.idx]) {
+          targetGraph[adj.idx].clear();
+          targetGraph[adj.idx].add(cur.idx);
           dist[adj.idx] = sum;
           pq.offer(new Node(adj.idx, sum));
         }
@@ -44,19 +46,14 @@ public class P5719 {
     }
   }
 
-  private static void bfs(final int START, final int END) {
-    Queue<Node> q = new LinkedList<>();
-    q.offer(new Node(END, 0));
-    while (!q.isEmpty()) {
-      Node cur = q.poll();
-      if (reverseGraph[cur.idx] == null) {
-        continue;
-      }
-      for (Node adj : reverseGraph[cur.idx]) {
-        if (dist[adj.idx] + adj.path == dist[cur.idx]) {
-          q.offer(adj);
-          dropped[adj.idx][cur.idx] = true;
-        }
+  private static void backTracking(int start, int sIdx) {
+    if (start == sIdx) {
+      return;
+    }
+    for (int next : targetGraph[sIdx]) {
+      if (!dropped[next][sIdx]) {
+        dropped[next][sIdx] = true;
+        backTracking(start, next);
       }
     }
   }
@@ -67,21 +64,24 @@ public class P5719 {
 
     while (true) {
       String[] input = br.readLine().split(" ");
-      final int N = Integer.parseInt(input[0]);
-      final int M = Integer.parseInt(input[1]);
-      if (N == 0 && M == 0) {
+      int n = Integer.parseInt(input[0]);
+      int m = Integer.parseInt(input[1]);
+      if (n == 0 && m == 0) {
         break;
       }
-      dist = new int[N];
-      graph = new ArrayList[N];
-      reverseGraph = new ArrayList[N];
-      dropped = new boolean[N][N];
+      dist = new int[n];
+      graph = new ArrayList[n];
+      targetGraph = new ArrayList[n];
+      dropped = new boolean[n][n];
+      for (int i = 0; i < n; i++) {
+        graph[i] = new ArrayList<>();
+        targetGraph[i] = new ArrayList<>();
+      }
 
       input = br.readLine().split(" ");
-      final int START = Integer.parseInt(input[0]);
-      final int END = Integer.parseInt(input[1]);
-
-      for (int i = 0; i < M; i++) {
+      int start = Integer.parseInt(input[0]);
+      int end = Integer.parseInt(input[1]);
+      for (int i = 0; i < m; i++) {
         input = br.readLine().split(" ");
         int u = Integer.parseInt(input[0]);
         int v = Integer.parseInt(input[1]);
@@ -92,12 +92,12 @@ public class P5719 {
         graph[u].add(new Node(v, p));
       }
 
-      dijkstra(N, START);
-      bfs(START, END);
-      dijkstra(N, START);
+      dijkstra(n, start);
+      backTracking(start, end);
+      dijkstra(n, start);
 
-      if (dist[END] != Integer.MAX_VALUE) {
-        bw.write(dist[END] + "\n");
+      if (dist[end] != Integer.MAX_VALUE) {
+        bw.write(dist[end] + "\n");
       } else {
         bw.write(-1 + "\n");
       }
@@ -110,16 +110,16 @@ public class P5719 {
 
   private static class Node implements Comparable<Node> {
     public int idx;
-    public int path;
+    public int weight;
 
-    public Node(int idx, int path) {
+    public Node(int idx, int weight) {
       this.idx = idx;
-      this.path = path;
+      this.weight = weight;
     }
 
     @Override
     public int compareTo(Node o) {
-      return this.path - o.path;
+      return this.weight - o.weight;
     }
   }
 }
