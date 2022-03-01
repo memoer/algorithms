569017fd3d70165fc80d3ea999d0e882ae105043
memoer <hanjn2842@naver.com>
2022-03-01 22:24:03 +0900

    [백준] 고급그래프>우주신과의 교감 [1774]
    - 메모리를 덜 차지하는 쪽으로 풀어봄
    - Map대신, [] 배열을 사용
    - 기존 크루스칼은 동일하게 사용

--- src/baekjoon/고급그래프/P1774.java
+++ src/baekjoon/고급그래프/P1774.java
@@ -1,102 +1,104 @@
 package baekjoon.고급그래프;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class P1774 {
-  private static class Edge {
-    public int a;
-    public int b;
-    public double weight;
-
-    public Edge(int a, int b, double weight) {
-      this.a = a;
-      this.b = b;
-      this.weight = weight;
-    }
-  }
-
-  private static Map<Integer, Integer> parent = new HashMap<>();
-  private static Map<Integer, Integer> rank = new HashMap<>();
-
-  private static double getWeight(int[] a, int[] b) {
-    int x = a[0] - b[0];
-    int y = a[1] - b[1];
-    return Math.sqrt((x * x) + (y * y));
-  }
-
-  private static int find(int i) {
-    if (i != parent.get(i)) {
-      parent.put(i, find(parent.get(i)));
-    }
-    return parent.get(i);
-  }
-
-  private static void union(int rootA, int rootB) {
-    int rankA = rank.get(rootA);
-    int rankB = rank.get(rootB);
-    if (rankA > rankB) {
-      parent.put(rootB, rootA);
-    } else {
-      parent.put(rootA, rootB);
-      if (rankA == rankB) {
-        rank.put(rootB, rankB + 1);
-      }
-    }
-  }
+  private static int[] parent;
+  private static int[] rank;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken());
-    int m = Integer.parseInt(st.nextToken());
-    List<int[]> list = new ArrayList<>();
-    List<Edge> graph = new ArrayList<>();
+    final int N = Integer.parseInt(st.nextToken()) + 1;
+    final int M = Integer.parseInt(st.nextToken());
+    int[][] locationArr = new int[N][2];
+    parent = new int[N];
+    rank = new int[N];
+    List<Edge> edgeList = new ArrayList<>();
     double result = 0;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i < N; i++) {
       st = new StringTokenizer(br.readLine());
-      int x = Integer.parseInt(st.nextToken());
-      int y = Integer.parseInt(st.nextToken());
-      list.add(new int[] { x, y });
+      locationArr[i][0] = Integer.parseInt(st.nextToken());
+      locationArr[i][1] = Integer.parseInt(st.nextToken());
+      parent[i] = i;
     }
-    for (int i = 0; i < n - 1; i++) {
-      for (int j = i + 1; j < n; j++) {
-        graph.add(new Edge(i + 1, j + 1, getWeight(list.get(i), list.get(j))));
-      }
-    }
-    for (int i = 1; i < n + 1; i++) {
-      parent.put(i, i);
-      rank.put(i, 0);
-    }
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < M; i++) {
       st = new StringTokenizer(br.readLine());
-      int rootA = find(Integer.parseInt(st.nextToken()));
-      int rootB = find(Integer.parseInt(st.nextToken()));
-      union(rootA, rootB);
+      union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
     }
     br.close();
 
-    graph.sort((pre, cur) -> {
-      if (pre.weight < cur.weight) {
-        return -1;
-      }
-      return 1;
-    });
-    for (Edge edge : graph) {
-      int rootA = find(edge.a);
-      int rootB = find(edge.b);
-      if (rootA != rootB) {
-        union(rootA, rootB);
-        result += edge.weight;
+    for (int i = 1; i < N - 1; i++) {
+      for (int j = i + 1; j < N; j++) {
+        edgeList.add(new Edge(i, j, getDistance(locationArr[i], locationArr[j])));
       }
     }
-    System.out.printf("%.2f\n", result);
+    Collections.sort(edgeList);
+
+    for (Edge edge : edgeList) {
+      if (find(edge.me) != find(edge.adj)) {
+        union(edge.me, edge.adj);
+        result += edge.path;
+      }
+    }
+    bw.write(String.format("%.2f", result) + "\n");
+    bw.flush();
+    bw.close();
+  }
+
+  private static double getDistance(int[] a, int[] b) {
+    return Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
+  }
+
+  private static int find(int node) {
+    if (node != parent[node]) {
+      parent[node] = find(parent[node]);
+    }
+    return parent[node];
+  }
+
+  private static void union(int nodeA, int nodeB) {
+    int rootA = find(nodeA);
+    int rootB = find(nodeB);
+    if (rootA == rootB) {
+      return;
+    }
+    int rankA = rank[rootA];
+    int rankB = rank[rootB];
+    if (rankA > rankB) {
+      parent[rootB] = rootA;
+    } else {
+      parent[rootA] = rootB;
+      if (rankA == rankB) {
+        rank[rootB] = rankB + 1;
+      }
+    }
+  }
+
+  private static class Edge implements Comparable<Edge> {
+    public int me;
+    public int adj;
+    public double path;
+
+    public Edge(int me, int adj, double path) {
+      this.me = me;
+      this.adj = adj;
+      this.path = path;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+      return Double.compare(this.path, o.path);
+    }
   }
 }
